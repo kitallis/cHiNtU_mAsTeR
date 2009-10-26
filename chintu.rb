6a8c8ba7f26c7cd0da830679c23399cd854a18ec
Akshay Gupta <kitallis@gmail.com>
2009-10-26 18:59:17 +0530

    new template support + now using the FileUtils lib

--- chintu.rb
+++ chintu.rb
@@ -1,5 +1,5 @@
 # Chintu's purpose in life is to remain single. Be small, stay compressed, as a working hack, although
-# making sure that he is nicely expandable if needed for solving multiple problems later on. 
+# making sure that he is nicely expandable if needed for solving multiple problems later on.
 #
 # He is usually a bit shy. He does get a little verbose when you annoy him too much. But that's okay.
 # He's rumoured to have been heavily inspired by pie-man's values in life.
@@ -154,26 +154,31 @@ a:hover {
   line-height: 20px;
 }
 EOS
-%w[rubygems rdiscount erb optparse yaml].map{|r|require r};
-class Template;def initialize(hash) @version = "Version 1.0" 
-hash.each_pair{|k, v| instance_variable_set('@' + k.to_s, v)} end 
+%w[rubygems rdiscount erb optparse yaml fileutils].map{|r|require r};
+class Template;def initialize(hash) @version = "Version 1.0"
+hash.each_pair{|k, v| instance_variable_set('@' + k.to_s, v)} end
 def get_binding ;binding end end
-module Setup A=proc{|x|Dir.chdir(x)};C=ARGV[0];B=proc{|f,w| File.open(f,'w'){|o| o.write w}}
-def self.dirCreate;Dir.mkdir(C);A[C]
-['themes','posts','out'].map{|a|puts"Creating /%s"%a;Dir.mkdir(a)}end
-def self.fileCreate ;B['blog.yaml',DY];B['themes/style.css',DS]
-B['themes/index.html',TEMPLATE.gsub(/^  /, '')]end 
-def self.writeStatic ;Dir["themes/*css"].each{|file|B["#{Misc.slug(file,"themes/")}",File.read(file)]}
-end end;class Metadata < Hash;protected;attr_accessor :DELIM;DELIM = /\A\s*\+-----\+(.+?)\+-----\+/m 
+module Setup C=ARGV[0];B=proc{|f,w| File.open(f,'w'){|o| o.write w}}
+def self.dirCreate ;Dir.mkdir(C);Dir.chdir(C);FileUtils.mkdir %w(themes themes/default posts out) end
+def self.fileCreate ;B['blog.yaml',DY];B['themes/default/style.css',DS]
+B['themes/default/index.html',TEMPLATE.gsub(/^  /, '')] end
+def self.writeStatic(theme) 
+Dir["themes/#{theme.to_s}/*.css"].each{|file| puts "Pushing static file from <- "+file.to_s;
+B["#{Misc.slug(file,"themes/#{theme.to_s}")}",File.read(file)]}
+end ;def self.readTemplate(theme);Dir["themes/#{theme.to_s}/index.html"].map{|file|File.read(file)}
+end end;class Metadata < Hash;protected;attr_accessor :DELIM;DELIM = /\A\s*\+-----\+(.+?)\+-----\+/m
 def self.parse(str) if str =~ DELIM;$1.strip.split("\n").inject(Metadata.new){
 |meta, line|k,v = line.split(":", 2);meta[k.strip.to_sym] = v.strip;meta}
-else Metadata.new end end end;class Post < Metadata;@@post = "";def self.parse(str) 
+else Metadata.new end end end;class Post < Metadata;@@post = "";def self.parse(str)
 super @@post = str.gsub(DELIM,'');RDiscount.new(@@post).to_html end end
-module Misc def self.check;if test ?e, 'blog.yaml';else puts "blog.yaml not found, exiting."
-Process.exit end end;def self.slug(str,path) str.to_s.gsub(path,"out/") end end
-module Spit;include Setup ;de = "";def self.main;Misc.check;Setup.writeStatic
-files = Dir["posts/*.post"].each{|file| test(?f, file)};html = ERB.new(TEMPLATE)
+module Misc ;def self.check ;if test ?e, yamlFile='blog.yaml';config = YAML.load_file(yamlFile) 
+theme = config["theme"];puts "Using the " + theme + " theme";theme.to_s ;else 
+puts "blog.yaml not found, exiting.";Process.exit end 
+end;def self.slug(str,path) str.to_s.gsub(path,"out/") end end
+module Spit;include Setup ;def self.main ;theme = Misc.check ;Setup.writeStatic(theme)
+if theme != "default" ;templates = Setup.readTemplate(theme) else ;templates = TEMPLATE end
+files = Dir["posts/*.post"].each{|file| test(?f, file)};html = ERB.new(templates.to_s)
 files.each{|file| content = File.read(file);meta = Metadata.parse(content)
-parse_meta = Template.new(meta);post = Post.parse(content);puts "Posting " + "#{file}" + "\n"
+parse_meta = Template.new(meta);post = Post.parse(content);puts "Posting post from <- " + "#{file}" + "\n"
 B["#{Misc.slug(file,"posts/")}.html",html.result(parse_meta.get_binding{post})]}end end
 if __FILE__ == $0 ;if ARGV[0]=="--compile"then Spit.main else Setup.dirCreate;Setup.fileCreate end end
